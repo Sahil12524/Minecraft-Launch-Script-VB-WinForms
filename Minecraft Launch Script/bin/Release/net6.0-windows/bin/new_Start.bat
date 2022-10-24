1e250c6e0b833cc71b181d4d754dc3ef4785e2da
sahil <sahil@DESKTOP-N494QAT>
2022-10-25 01:09:45 +0530

    Added more processes to be killed while loading bypass and stopping bypass, (Leads to single click load and stop of bypass).

--- Minecraft Launch Script/bin/Release/net6.0-windows/bin/new_Start.bat
+++ Minecraft Launch Script/bin/Release/net6.0-windows/bin/new_Start.bat
@@ -6,12 +6,19 @@ set curdir=%~dp0
 echo %curdir%
 ::del "%curdir%\dlls\Original Dlls Backup\System32\Windows.ApplicationModel.Store.dll"
 ::del "%curdir%dlls\Original Dlls Backup\sysWOW64\Windows.ApplicationModel.Store.dll"
+taskkill /F /IM "Minecraft.Windows.exe"
 taskkill /F /IM "GameBar.exe"
 taskkill /F /IM "GameBarFTServer.exe"
 taskkill /F /IM "GameBarPresenceWriter.exe"
 taskkill /F /IM "gameinputsvc.exe"
 taskkill /F /IM "gamingservices.exe"
-taskkill /F /IM "Minecraft.Windows.exe"
+taskkill /F /IM "XboxIdp.exe"
+taskkill /F /IM "WWAHost.exe"
+taskkill /F /IM "RuntimeBroker.exe"
+taskkill /F /IM "StoreExperienceHost.exe"
+taskkill /F /IM "Cortana.exe"
+taskkill /F /IM "SearchHost.exe"
+taskkill /F /IM "StartMenuExperienceHost.exe"
 del /f "C:\Windows\System32\Windows.ApplicationModel.Store.dll"
 ::timeout 3 /nobreak
 cd /
--- Minecraft Launch Script/bin/Release/net6.0-windows/bin/new_Stop.bat
+++ Minecraft Launch Script/bin/Release/net6.0-windows/bin/new_Stop.bat
@@ -4,12 +4,19 @@ color 0a
 title New Stop
 set curdir=%~dp0
 echo %curdir%
+taskkill /F /IM "Minecraft.Windows.exe"
 taskkill /F /IM "GameBar.exe"
 taskkill /F /IM "GameBarFTServer.exe"
 taskkill /F /IM "GameBarPresenceWriter.exe"
 taskkill /F /IM "gameinputsvc.exe"
 taskkill /F /IM "gamingservices.exe"
-taskkill /F /IM "Minecraft.Windows.exe"
+taskkill /F /IM "XboxIdp.exe"
+taskkill /F /IM "WWAHost.exe"
+taskkill /F /IM "RuntimeBroker.exe"
+taskkill /F /IM "StoreExperienceHost.exe"
+taskkill /F /IM "Cortana.exe"
+taskkill /F /IM "SearchHost.exe"
+taskkill /F /IM "StartMenuExperienceHost.exe"
 takeown /f c:\windows\system32\Windows.ApplicationModel.Store.dll
 icacls c:\windows\system32\Windows.ApplicationModel.Store.dll /GRANT ADMINISTRATORS:F
 del "C:\Windows\System32\Windows.ApplicationModel.Store.dll"
